e76e489d14c5dc18a7f4daa429929efcc42ecda3
Will Mavis <willard.mavis@gmail.com>
2018-07-12 16:43:37 -0500

    Add some code to models from question

--- app/models/guide_availability.rb
+++ app/models/guide_availability.rb
@@ -1,2 +1,8 @@
 class GuideAvailability < ApplicationRecord
+  has_many :guide_availability_activities, dependent: :destroy
+  has_many :activities, through: :guide_availability_activities, dependent: :destroy
+  has_many :guide_availability_locations, dependent: :destroy
+  has_many :locations, through: :guide_availability_locations, dependent: :destroy
+  has_many :guide_availability_options, dependent: :destroy
+  has_many :options, through: :guide_availability_options, dependent: :destroy
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,6 @@
 class Trip < ApplicationRecord
+  has_many :trip_activities
+  has_many :activities, through: :trip_activities
+  has_many :trip_options
+  has_many :options, through: :trip_options
 end
